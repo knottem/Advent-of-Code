65714c1bd2af0c2f35ae31e780c377e78ae675b9
knottem <59744487+knottem@users.noreply.github.com>
2023-02-23 18:17:22 +0100

    Update Day01.kt
    just cleaned it up a bit

--- src/main/year2019/day01/Day01.kt
+++ src/main/year2019/day01/Day01.kt
@@ -7,7 +7,7 @@ class Day01(text : String = "input.txt") {
     private val day = "01"
     private val year = "2019"
 
-    private val input = File("src/resources/$year/$day/$text").readLines().toList().map { it.toInt() }.toIntArray()
+    private val input = File("src/resources/$year/$day/$text").readLines().map { it.toInt() }.toList()
 
     //getting the sum as int, so we can round it down to the nearest Int and we get the correct amount of fuel
     fun part1(): Int = input.sumOf { it / 3 - 2 }
@@ -17,4 +17,3 @@ class Day01(text : String = "input.txt") {
     private fun fuel(it: Int) : Int = if (it / 3 - 2 > 0) it / 3 - 2 + fuel(it / 3 - 2) else 0
 
 }
-
