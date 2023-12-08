472362d64b21e9b175e91ed3347bee1330d77581
Erik <59744487+knottem@users.noreply.github.com>
2023-12-08 17:58:02 +0100

    part 1 complete day 7, cleaned up the code a bit

--- src/main/year2023/Day07.java
+++ src/main/year2023/Day07.java
@@ -24,35 +24,22 @@ public class Day07 extends Day {
             else if (Character.isDigit(c)) cardsList.add(Character.getNumericValue(c));
         }
         List<Integer> copy = new ArrayList<>(cardsList);
+        int[] result = new int[]{0, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
         // Sort the ranks by frequency, then by value.
-        cardsList.sort(Comparator.comparingInt((Integer o) -> Collections.frequency(cardsList, o)).thenComparingInt(o -> o));
+        cardsList.sort(Comparator.comparingInt((Integer o) ->
+                Collections.frequency(cardsList, o)).thenComparingInt(o -> o));
 
-        if (part2) {
-            // Check if J cards can form a stronger hand.
-            int maxRank = Collections.max(cardsList);
-            for (int i = 0; i < cardsList.size(); i++) {
-                if (cardsList.get(i) == -1) {
-                    cardsList.set(i, maxRank + 1); // Treat J as the best possible card.
-                }
-            }
-            cardsList.sort(Comparator.comparingInt((Integer o) -> Collections.frequency(cardsList, o)).thenComparingInt(o -> o));
-        }
         // Check for the various hands.
-        if (IntStream.rangeClosed(0, 4).mapToObj(cardsList::get).distinct().count() == 1) { // Five of a kind
-            return new int[]{10, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        } else if (IntStream.rangeClosed(1, 4).mapToObj(cardsList::get).distinct().count() == 1) { // Four of a kind
-            return new int[]{9, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        } else if (Objects.equals(cardsList.get(0), cardsList.get(1)) && IntStream.rangeClosed(2, 4).mapToObj(cardsList::get).distinct().count() == 1){ // Full house
-            return new int[]{8, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        } else if (IntStream.rangeClosed(2, 4).mapToObj(cardsList::get).distinct().count() == 1) { // Three of a kind
-            return new int[]{7, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        } else if (Objects.equals(cardsList.get(1), cardsList.get(2)) && Objects.equals(cardsList.get(3), cardsList.get(4))) { // Two pair
-            return new int[]{6, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        } else if (Objects.equals(cardsList.get(3), cardsList.get(4))) { // Pair
-            return new int[]{5, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        } else { // High card
-            return new int[]{4, copy.get(0), copy.get(1), copy.get(2), copy.get(3), copy.get(4)};
-        }
+        if (IntStream.rangeClosed(0, 4).mapToObj(cardsList::get).distinct().count() == 1) result[0] = 10;
+        else if (IntStream.rangeClosed(1, 4).mapToObj(cardsList::get).distinct().count() == 1) result[0] = 9;
+        else if (Objects.equals(cardsList.get(0), cardsList.get(1))
+                && IntStream.rangeClosed(2, 4).mapToObj(cardsList::get).distinct().count() == 1) result[0] = 8;
+        else if (IntStream.rangeClosed(2, 4).mapToObj(cardsList::get).distinct().count() == 1) result[0] = 7;
+        else if (Objects.equals(cardsList.get(1), cardsList.get(2))
+                && Objects.equals(cardsList.get(3), cardsList.get(4))) result[0] = 6;
+        else if (Objects.equals(cardsList.get(3), cardsList.get(4))) result[0] = 5;
+        else result[0] = 4;
+        return result;
     }
 
     private List<CardsData> createList() {
