0a1427c21419d29d7cdfcefca649a3f2ee871db5
Erik Wallenius <walle.o.w@gmail.com>
2024-12-04 12:55:57 +0100

    day 4 part 2 took quite a while, not the cleanest solution, but it works.

--- src/main/year2024/Day03.java
+++ src/main/year2024/Day03.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 public class Day03 extends Day {
 
     Pattern pattern;
+    Matcher matcher;
 
     public Day03() {
         super("input", "03", "2024");
@@ -25,6 +26,7 @@ public class Day03 extends Day {
 
     // Add support for do() (enables) and don't() (disables) instructions to control mul(X,Y) processing.
     // At start, mul is enabled; only enabled instructions contribute to the sum.
+    // regex is king for this, since we can just replace everything between don't and do
     @Override
     public long part2() {
         String cleanedInput = String.join("", getInput()).replaceAll("don't\\(\\).*?do\\(\\)|don't\\(\\).*$", "");
@@ -33,7 +35,7 @@ public class Day03 extends Day {
 
     private long multiply (String input) {
         long result = 0;
-        Matcher matcher = pattern.matcher(input);
+        matcher = pattern.matcher(input);
         while (matcher.find()) {
             result += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
         }
