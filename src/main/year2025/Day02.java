8cb465bb3dd5d854f32a77440e1b0c393fb35838
Erik Wallenius <walle.o.w@gmail.com>
2025-12-02 21:22:11 +0100

    Day2 2025 - Part 1 and 2 completed

---
+++ src/main/year2025/Day02.java
@@ -0,0 +1,73 @@
+package year2025;
+
+import template.Day;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day02 extends Day {
+
+    public Day02(){
+        super("input", "02", "2025");
+    }
+
+    @Override
+    public long part1() {
+        long invalidIds = 0;
+        List<long[]> ranges = getRanges();
+        for (long[] range : ranges) {
+            for (long i =  range[0]; i <= range[1]; i++) {
+                if(invalidNumber(String.valueOf(i))){
+                    invalidIds += i;
+                }
+            }
+        }
+        return invalidIds;
+    }
+
+    private boolean invalidNumber(String number) {
+        if(number.startsWith("0")) return true;
+        String firstHalf = number.substring(0, number.length() / 2);
+        String secondHalf = number.substring(number.length() / 2);
+        return firstHalf.equals(secondHalf);
+    }
+
+    @Override
+    public long part2() {
+        long invalidIds = 0;
+        List<long[]> ranges = getRanges();
+        for (long[] range : ranges) {
+            for (long i =  range[0]; i <= range[1]; i++) {
+                if(invalidNumberPart2V1(String.valueOf(i))){
+                    invalidIds += i;
+                }
+            }
+        }
+        return invalidIds;
+    }
+
+    public static boolean invalidNumberPart2V1(String number) {
+        int length = number.length();
+        for (int block = 1; block <= length / 2; block++) {
+            if (length % block != 0) continue;
+            StringBuilder string = new StringBuilder();
+            for (long i = 0; i < length / block; i++) string.append(number, 0, block);
+            if (string.toString().equals(number)) return true;
+        }
+        return false;
+    }
+
+    private boolean invalidNumberPart2V2(String number) {
+        return (number + number).indexOf(number, 1) != number.length();
+    }
+
+    private List<long[]> getRanges(){
+        return Arrays.stream(getInput().get(0).trim()
+                .split(","))
+                .map(s -> s.split("-"))
+                .map(a -> new long[]{ Long.parseLong(a[0]), Long.parseLong(a[1]) })
+                .toList();
+    }
+
+
+}
