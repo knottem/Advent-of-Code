82387ed4f1b8ab1c2d9400a9078d63036b5a7b22
Erik Wallenius <walle.o.w@gmail.com>
2025-12-03 19:18:19 +0100

    Day3 2025 - Part 2 Completed - refractored my first solution so it works with more amounts and use StringBuilder instead to make the max number

--- src/main/year2025/Day03.java
+++ src/main/year2025/Day03.java
@@ -18,31 +18,36 @@ public class Day03 extends Day {
 
     @Override
     public long part1() {
-        long total = 0;
-        for (List<Integer> list : convertedList) {
-            int max = 0;
-            int maxIndex = 0;
-            int secondMax = 0;
-            for (int i = 0; i < list.size() - 1; i++) {
-                if (list.get(i) > max) {
-                    max = list.get(i);
-                    maxIndex = i;
-                }
-            }
-            for (int i = maxIndex + 1; i < list.size() ; i++) {
-                if (list.get(i) > secondMax) {
-                    secondMax = list.get(i);
-                }
-            }
-            String s1 = String.valueOf(max);
-            String s2 = String.valueOf(secondMax);
-            total += Integer.parseInt(s1+s2);
-        }
-        return total;
+        return totalJoltage(2);
     }
 
     @Override
     public long part2() {
-        return 0;
+        return totalJoltage(12);
+    }
+
+
+    public long totalJoltage(int amount) {
+        long total = 0;
+        for (List<Integer> list : convertedList) {
+            int startIndex = 0;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < amount; i++) {
+                int max = -1;
+                int maxIndex = startIndex;
+                int lastSearchIndex = list.size() - (amount - i);
+                for (int j = startIndex; j <= lastSearchIndex; j++) {
+                    if (list.get(j) > max) {
+                        max = list.get(j);
+                        maxIndex = j;
+                    }
+                }
+                sb.append(max);
+                startIndex = maxIndex + 1;
+            }
+            total += Long.parseLong(sb.toString());
+        }
+
+        return total;
     }
 }
